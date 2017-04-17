daa1427e579da0e0450842d1c625783f8f649c4b
harsha49970 <harsha49970@gmail.com>
2017-04-17 17:08:03 -0400

    updating modules

---
+++ contrib/bootstrap_site_alert/js/bootstrap-site-alert.js
@@ -0,0 +1,34 @@
+/**
+ * @file
+ *
+ * JavaScript functions for Bootstrap Site Alert module.
+ */
+
+(function ($) {
+  Drupal.behaviors.bootstrapSiteAlert = {
+    attach: function (context, settings) {
+
+      // Since the key is updated every time the configuration form is saved,
+      // we can ensure users don't miss newly added or changed alerts.
+      var key = Drupal.settings.bootstrap_site_alert.key;
+
+      // Only show the alert if dismiss button has not been clicked. The
+      // element is hidden by default in order to prevent it from momentarily
+      // flickering onscreen. We are not working with Bootstrap's 'hide' class
+      // since we don't want a dependency on Bootstrap.
+      if ($.cookie('Drupal.visitor.bootstrap_site_alert_dismissed') !== key) {
+        $('.bs-site-alert').css('display', 'block');
+      }
+
+      // Set the cookie value when dismiss button is clicked.
+      $('.bs-site-alert .close').click(function(e) {
+
+        // Do not perform default action.
+        e.preventDefault();
+
+        // Set cookie to the current key.
+        $.cookie('Drupal.visitor.bootstrap_site_alert_dismissed', key, { path: Drupal.settings.basePath });
+      });
+    }
+  }
+})(jQuery);
---
+++ contrib/site_alert/templates/site-alert-wrapper.tpl.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @file
+ * site-alert-wrapper.tpl.php
+ */
+?>
+<div class="site-alert"></div>
