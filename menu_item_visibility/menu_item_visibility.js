6ad80cbceb12460a6d437af720ad0e9ea241b67f
Sriharsha Chirumamilla <harsha49970@gmail.com>
2018-03-12 11:57:47 -0400

    Implemented Updates

--- menu_item_visibility/menu_item_visibility.js
+++ menu_item_visibility/menu_item_visibility.js
@@ -5,26 +5,6 @@
  */
 Drupal.behaviors.menuItemVisibilitySummary = {
   attach: function (context) {
-    /*$('fieldset#edit-path', context).drupalSetSummary(function (context) {
-      if (!$('textarea[name="pages"]', context).val()) {
-        return Drupal.t('Not restricted');
-      }
-      else {
-        return Drupal.t('Restricted to certain pages');
-      }
-    });*/
-
-    /*$('fieldset#edit-node-type', context).drupalSetSummary(function (context) {
-      var vals = [];
-      $('input[type="checkbox"]:checked', context).each(function () {
-        vals.push($.trim($(this).next('label').text()));
-      });
-      if (!vals.length) {
-        vals.push(Drupal.t('Not restricted'));
-      }
-      return vals.join(', ');
-    });*/
-
     $('fieldset#edit-role', context).drupalSetSummary(function (context) {
       var vals = [];
       $('input[type="checkbox"]:checked', context).each(function () {
